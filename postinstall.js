16f880739e804457a2148c694c99b1e4639b5aa9
Gregory Smith, II <Greg.W.Smith@nordstrom.com>
2016-10-18 20:36:47 -0700

    Add name to lambda package.json in attempts to prevent npm error while installing.

--- postinstall.js
+++ postinstall.js
@@ -1,4 +1,7 @@
-const join = require('path').join;
-
 // Spawn `npm -install` to populate our Lmabda's dependencies.
-require('child_process').spawn('npm', ['i'], { env: process.env, cwd: join(__dirname, 'lib', 'lambda'), stdio: 'inherit' });
+require('child_process')
+    .spawn('npm', ['i'], {
+        env: process.env,
+        cwd: require('path').join(__dirname, 'lib', 'lambda'),
+        stdio: 'inherit'
+    });
