21998a1e47f403043485bec1c1e6cafdbf002541
Gregory Smith, II <Greg.W.Smith@nordstrom.com>
2016-10-19 19:24:41 -0700

    Use dependencies.json to install each of the packages needed for Lambda.

--- postinstall.js
+++ postinstall.js
@@ -1,7 +1,20 @@
 // Spawn `npm -install` to populate our Lmabda's dependencies.
-require('child_process')
-    .spawn('npm', ['i'], {
+'use strict';
+const cp = require('child_process'),
+      join = require('path').join,
+      dependencies = JSON.parse(
+          require('fs').readFileSync(
+              __dirname + '/lib/lambda/dependencies.json',
+              { encoding: 'utf8' }
+          )
+      ).dependencies;
+
+for (let name in dependencies) {
+    let version = dependencies[name];
+    console.log(`Installing Lambda dependency: ${name}@${version}`);
+    cp.exec('npm', ['install', `${name}@${version}`], {
         env: process.env,
-        cwd: require('path').join(__dirname, 'lib', 'lambda'),
+        cwd: join(__dirname, 'lib', 'lambda'),
         stdio: 'inherit'
     });
+}
