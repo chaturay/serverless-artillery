7ee6553d8a5b758f68e00c252a6dde1dc3e0de43
Erik Erikson <erikerikson@users.noreply.github.com>
2016-10-22 20:57:33 -0700

    Perform an `npm install` after configure (#44)
    Since we do this during postinstall too, consolidate the logic to make it reusable in both contexts.
    Thereby, add ~/lib/npm.js
    Use that in ~/postinstall.js
    Use that in ~/lib/index.js for the 'configure' command

--- postinstall.js
+++ postinstall.js
@@ -1,17 +1,9 @@
-// Spawn `npm -install` to populate our Lambda's dependencies.
-
 'use strict';
 
-const execSync = require('child_process').execSync;
-const join = require('path').join;
+const path = require('path');
 
-// copy and clear the "global" aspect of the process environment (this way, pass proxy settings and whatnot through)
-const env = JSON.parse(JSON.stringify(process.env));
-delete env.npm_config_argv;
-delete env.npm_config_global;
+const npm = require('./lib/npm');
 
-execSync('npm install', {
-  env,
-  cwd: join(__dirname, 'lib', 'lambda'),
-  stdio: 'inherit',
-});
+const installPath = path.join(__dirname, 'lib', 'lambda');
+
+npm.install(installPath); // will throw out and fail install if this times out or returns a non-zero result
