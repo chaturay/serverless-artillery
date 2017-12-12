735c5b8f91792e0d6d7c23a9c9886243cb90bb39
Charles Sullivan <sullux@gmail.com>
2017-12-12 00:25:29 +0000

    create first-pass functional version of configure module with unit tests

---
+++ tests/lib/configurator.spec.js
@@ -0,0 +1,235 @@
+const assert = require('assert')
+const { stub } = require('sinon')
+const configurator = require('../../lib/configurator')
+const { freeze } = require('../../lib/fp')
+
+const { serviceName, messages, serverlessFiles } = configurator
+const randomValue = 'ABC123'
+
+describe('lib/configurator', () => {
+  describe('configure', () => {
+    context('with successful file write and npm install', () => {
+      const fileData = `file data ${serviceName} more file data`
+      let sideEffects
+      beforeEach(() => {
+        sideEffects = freeze({
+          log: stub(),
+          randomid: stub().returns(randomValue),
+          writeFileIfNotExists: stub().returns(Promise.resolve(true)),
+          readLambdaFile: stub().returns(Promise.resolve(fileData)),
+          deleteFile: stub().returns(Promise.resolve()),
+          npmInstall: stub().returns(Promise.resolve()),
+        })
+        return configurator(sideEffects)()
+      })
+
+      const expectedLogs = [[messages.complete]]
+      it('should log status messages', () =>
+        assert.deepStrictEqual(sideEffects.log.args, expectedLogs))
+
+      const expectedFileData =
+        `file data ${serviceName}-${randomValue} more file data`
+      it('should write files', () => {
+        const writeFileIfNotExists = sideEffects.writeFileIfNotExists
+        serverlessFiles.map(file =>
+          assert(writeFileIfNotExists.calledWith(file, expectedFileData)))
+      })
+
+      it('should NPM install', () =>
+        assert.strictEqual(sideEffects.npmInstall.callCount, 1))
+    })
+
+    context('debug with successful file write and npm install', () => {
+      const fileData = `file data ${serviceName} more file data`
+      let sideEffects
+      beforeEach(() => {
+        sideEffects = freeze({
+          log: stub(),
+          randomid: stub().returns(randomValue),
+          writeFileIfNotExists: stub().returns(Promise.resolve(true)),
+          readLambdaFile: stub().returns(Promise.resolve(fileData)),
+          deleteFile: stub().returns(Promise.resolve()),
+          npmInstall: stub().returns(Promise.resolve()),
+        })
+        return configurator(sideEffects)({ debug: true })
+      })
+
+      const expectedLogs = [
+        [messages.identifying],
+        [messages.noConflict],
+        [messages.npmInstall],
+        [messages.complete],
+      ]
+      it('should log status and debug messages', () =>
+        assert.deepStrictEqual(sideEffects.log.args, expectedLogs))
+
+      const expectedFileData =
+        `file data ${serviceName}-${randomValue} more file data`
+      it('should write files', () => {
+        const writeFileIfNotExists = sideEffects.writeFileIfNotExists
+        serverlessFiles.map(file =>
+          assert(writeFileIfNotExists.calledWith(file, expectedFileData)))
+      })
+
+      it('should NPM install', () =>
+        assert.strictEqual(sideEffects.npmInstall.callCount, 1))
+    })
+
+    context('with successful file write and failed npm install', () => {
+      const fileData = `file data ${serviceName} more file data`
+      let sideEffects
+      beforeEach(() => {
+        sideEffects = freeze({
+          log: stub(),
+          randomid: stub().returns(randomValue),
+          writeFileIfNotExists: stub().returns(Promise.resolve(true)),
+          readLambdaFile: stub().returns(Promise.resolve(fileData)),
+          deleteFile: stub().returns(Promise.resolve()),
+          npmInstall: stub().returns(Promise.reject(new Error('failed'))),
+        })
+        return configurator(sideEffects)()
+      })
+
+      const expectedLogs = [[messages.complete], [messages.npmInstallError]]
+      it('should log status messages', () =>
+        assert.deepStrictEqual(sideEffects.log.args, expectedLogs))
+
+      const expectedFileData =
+        `file data ${serviceName}-${randomValue} more file data`
+      it('should write files', () => {
+        const writeFileIfNotExists = sideEffects.writeFileIfNotExists
+        serverlessFiles.map(file =>
+          assert(writeFileIfNotExists.calledWith(file, expectedFileData)))
+      })
+
+      it('should NPM install', () =>
+        assert.strictEqual(sideEffects.npmInstall.callCount, 1))
+
+      it('should clean up files', () => {
+        const deleteFile = sideEffects.deleteFile
+        serverlessFiles.forEach(file => assert(deleteFile.calledWith(file)))
+      })
+    })
+
+    context('debug with successful file write and failed npm install', () => {
+      const fileData = `file data ${serviceName} more file data`
+      let sideEffects
+      beforeEach(() => {
+        sideEffects = freeze({
+          log: stub(),
+          randomid: stub().returns(randomValue),
+          writeFileIfNotExists: stub().returns(Promise.resolve(true)),
+          readLambdaFile: stub().returns(Promise.resolve(fileData)),
+          deleteFile: stub().returns(Promise.resolve()),
+          npmInstall: stub().returns(Promise.reject(new Error('failed'))),
+        })
+        return configurator(sideEffects)({ debug: true })
+      })
+
+      const expectedLogs = [
+        [messages.identifying],
+        [messages.noConflict],
+        [messages.npmInstall],
+        [messages.complete],
+        [messages.npmInstallError],
+      ]
+      it('should log status and debug messages', () =>
+        assert.deepStrictEqual(sideEffects.log.args, expectedLogs))
+
+      const expectedFileData =
+        `file data ${serviceName}-${randomValue} more file data`
+      it('should write files', () => {
+        const writeFileIfNotExists = sideEffects.writeFileIfNotExists
+        serverlessFiles.map(file =>
+          assert(writeFileIfNotExists.calledWith(file, expectedFileData)))
+      })
+
+      it('should NPM install', () =>
+        assert.strictEqual(sideEffects.npmInstall.callCount, 1))
+
+      it('should clean up files', () => {
+        const deleteFile = sideEffects.deleteFile
+        assert.strictEqual(deleteFile.callCount, serverlessFiles.length)
+        serverlessFiles.forEach(file => assert(deleteFile.calledWith(file)))
+      })
+    })
+
+    context('with file conflicts', () => {
+      const fileData = `file data ${serviceName} more file data`
+      const conflictFiles = [serverlessFiles[0]]
+      const noConflictFiles = serverlessFiles.slice(1)
+      let sideEffects
+      beforeEach(() => {
+        sideEffects = freeze({
+          log: stub(),
+          randomid: stub().returns(randomValue),
+          writeFileIfNotExists: stub().callsFake(name =>
+            Promise.resolve(!conflictFiles.includes(name))),
+          readLambdaFile: stub().returns(Promise.resolve(fileData)),
+          deleteFile: stub().returns(Promise.resolve()),
+          npmInstall: stub().returns(Promise.resolve()),
+        })
+        return configurator(sideEffects)()
+      })
+
+      const expectedLogs = [[messages.conflictList(conflictFiles)]]
+      it('should log status messages', () =>
+        assert.deepStrictEqual(sideEffects.log.args, expectedLogs))
+
+      const expectedFileData =
+        `file data ${serviceName}-${randomValue} more file data`
+      it('should write files', () => {
+        const writeFileIfNotExists = sideEffects.writeFileIfNotExists
+        serverlessFiles.map(file =>
+          assert(writeFileIfNotExists.calledWith(file, expectedFileData)))
+      })
+
+      it('should clean up no-conflict files', () => {
+        const deleteFile = sideEffects.deleteFile
+        assert.strictEqual(deleteFile.callCount, noConflictFiles.length)
+        noConflictFiles.forEach(file => assert(deleteFile.calledWith(file)))
+      })
+    })
+
+    context('debug with file conflicts', () => {
+      const fileData = `file data ${serviceName} more file data`
+      const conflictFiles = [serverlessFiles[0]]
+      const noConflictFiles = serverlessFiles.slice(1)
+      let sideEffects
+      beforeEach(() => {
+        sideEffects = freeze({
+          log: stub(),
+          randomid: stub().returns(randomValue),
+          writeFileIfNotExists: stub().callsFake(name =>
+            Promise.resolve(!conflictFiles.includes(name))),
+          readLambdaFile: stub().returns(Promise.resolve(fileData)),
+          deleteFile: stub().returns(Promise.resolve()),
+          npmInstall: stub().returns(Promise.resolve()),
+        })
+        return configurator(sideEffects)({ debug: true })
+      })
+
+      const expectedLogs = [
+        [messages.identifying],
+        [messages.conflict],
+        [messages.conflictList(conflictFiles)],
+      ]
+      it('should log status messages', () =>
+        assert.deepStrictEqual(sideEffects.log.args, expectedLogs))
+
+      const expectedFileData =
+        `file data ${serviceName}-${randomValue} more file data`
+      it('should write files', () => {
+        const writeFileIfNotExists = sideEffects.writeFileIfNotExists
+        serverlessFiles.map(file =>
+          assert(writeFileIfNotExists.calledWith(file, expectedFileData)))
+      })
+
+      it('should clean up no-conflict files', () => {
+        const deleteFile = sideEffects.deleteFile
+        assert.strictEqual(deleteFile.callCount, noConflictFiles.length)
+        noConflictFiles.forEach(file => assert(deleteFile.calledWith(file)))
+      })
+    })
+  })
+})
---
+++ tests/lib/fp.spec.js
@@ -0,0 +1,95 @@
+const assert = require('assert')
+const {
+  pipe, tap, map, freeze, tapif,
+} = require('../../lib/fp')
+
+/* eslint-disable no-return-assign */
+/* eslint-disable no-param-reassign */
+
+describe('lib/fp', () => {
+  describe('freeze', () => {
+    it('should freeze an object', () => {
+      pipe(freeze({}), [
+        tap(frozen => (frozen.prop = true) || false), // eslint-disable-line no-param-reassign
+        tap(frozen => assert(!frozen.prop)),
+      ])
+    })
+
+    it('should freeze an array', () =>
+      assert.throws(() => freeze([10, 20]).push(1)))
+
+    it('should freeze nested members of complex object', () =>
+      pipe(freeze({
+        foo: {}, bar: [1], baz: 23, biz: new Date(),
+      }), [
+        tap(frozen => (frozen.foo.prop = true)), // eslint-disable-line no-param-reassign
+        tap(frozen => assert(!frozen.foo.prop)),
+        tap(frozen => assert.throws(() => frozen.bar.push(2))),
+      ]))
+
+    it('should freeze elements of array', () =>
+      pipe(freeze([
+        {}, [1], 23, new Date(),
+      ]), [
+        tap(frozen => frozen[0].prop = true),
+        tap(frozen => assert(!frozen[0].prop)),
+        tap(frozen => assert.throws(() => frozen[1].push(2))),
+      ]))
+  })
+
+  describe('pipe', () => {
+    it('should pipe', () =>
+      assert.strictEqual(pipe(1, [n => n + 1, n => n.toString()]), '2'))
+
+    it('should pipe with promises', () => pipe(1, [
+      i => i + 1,
+      i => Promise.resolve(i + 1),
+      i => i + 1,
+    ])
+      .then(i => assert.strictEqual(i, 4)))
+  })
+
+  describe('tap', () => {
+    it('should return the original value', () =>
+      assert.strictEqual(tap(n => n.toString())(2), 2))
+
+    it('should execute the side effect', () => {
+      let mutatable = 2
+      tap((n) => { mutatable = n })(3)
+      assert(mutatable, 3)
+    })
+  })
+
+  describe('tapif', () => {
+    const tapifExecutesSideEffect = (condition) => {
+      let mutatable = 2
+      tapif(condition)((n) => { mutatable = n })(3)
+      return mutatable === 3
+    }
+
+    const truthyValues = [true, 1, 'foo', {}, [], () => {}]
+    it('should execute the side effect when condition is truthy', () =>
+      truthyValues
+        .map(tapifExecutesSideEffect)
+        .forEach(assert))
+
+    const falsyValues = [undefined, null, false, 0, '']
+    it('should not execute the side effect when condition is falsy', () =>
+      falsyValues
+        .map(tapifExecutesSideEffect)
+        .forEach(hasSideEffect => assert(!hasSideEffect)))
+  })
+
+  describe('map', () => {
+    it('should map to a new array', () => {
+      const input = [10, 20]
+      assert.notStrictEqual(map(n => n.toString())(input), input)
+    })
+
+    it('should map to an array with the correct values', () =>
+      assert.deepStrictEqual(map(n => n.toString())([10, 20]), ['10', '20']))
+  })
+})
+
+/* eslint-enable no-return-assign */
+/* eslint-enable no-param-reassign */
