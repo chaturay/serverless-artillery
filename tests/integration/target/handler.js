10844c94e8f5cee71eaa78bb1ac6223f06a6e30e
Charles Sullivan <sullux@gmail.com>
2018-08-20 18:24:26 +0000

    add comments for clarity

--- tests/integration/target/handler.js
+++ tests/integration/target/handler.js
@@ -1,10 +1,13 @@
 const pure = {
+  // returns a random string of digits and lower-case characters
   randomString: (length, random = Math.random) =>
     [...Array(length)].map(i => (~~(random() * 36)).toString(36)).join(''),
 
+  // generates an event ID from a timestamp plus a random string
   createEventId: (now = Date.now, randomString = pure.randomString) =>
     `${now()}.${randomString()}`,
 
+  // abstract implementation of the aws lambda handler logic
   handler: impl =>
     (event, context, callback) => {
       try {
@@ -16,6 +19,7 @@ const pure = {
       }
     },
 
+  // the api GET handler implementation
   get: (createEventId = pure.createEventId) =>
     event =>
       Object.assign(
