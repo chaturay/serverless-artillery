66571f2cb689ad7685cee4b140ea6647173eb6f1
Gregory W. Smith II <gws_ii@msn.com>
2019-02-22 13:40:25 -0800

    Adds upgrade command and implements for 0.0.0 to 0.0.1 (#194)
    * Add Update Command
    
    Add update command to CLI
    Establish versioning folder to contain:
      1. old versions (for comparison)
      2. version specific update logic
    
    Please note that the migration of logic from ~/lib/index.js into the versioning folder was entirely incomplete, as was most of the thinking associated with design and factoring of this feature.
    
    Of particular note, this mostly implements the logic for taking an existing serverless.yml and non-destructively updating it with the monitoring mode assets.  Missing functionality included (as per #161) the updating of the package.json or of the handler's javascript files.  Update should be non-destructive, preserving any customizations that cannot be automatically transferred.
    
    * Implements validation of project dependencies for upgrade command in versioning module.
    
    * Adds validation of service definition.
    
    * Unit tests all passing for validateServiceConfiguration (in upgrade context)  and validateServiceForDeployment.
    
    * Renames asset directories by version number.
    Makes local assets path a higher order parameter to versioning implementation.
    Corrected update -> upgrade.
    Relocate validateServiceForUpgrade function and test.
    
    * Uses AJV and JSON schema definitions capture required preconditions and naming conflicts in the function service definition (serverless.yml).
    
    * Implements the version by version upgrade by plugin mechanism.
    
    * Removes now deprecated upgrade code from lib/index.js.
    
    * Removes deprecated constants.
    
    * Adds README to explain
    
    * WIP: Upgrade saves to directories.
    
    * Fix bugs preventing proper backup of project files and upgrade of package.json.
    Establishes baseline versioning upgrade integration tests from 0.0.0 to 0.0.1.
    
    * Repair some unit test drift.
    
    * Refactor upgradeTest function to be more comprehensible.
    
    * Fix typos and update versioning README to describe the revised backup mechanism.
    Adds manual upgrade script for testing.
    
    * Remove unnecessary debug logging.
    
    * Adds output during upgrade to list files which were backed up.
    
    * Minor adjustment to upgrade output.
    
    * More verbose output explaining the next steps for users to finalize project upgrade. Improved output in general.

---
+++ tests/integration/versioning/0.0.0-to-0.0.1/default/upgraded/funcValid.js
@@ -0,0 +1,69 @@
+const def = require('./funcDef')
+
+const impl = {
+  /**
+   * Validate the given event's function relevant configuration, throwing an exception if invalid configuration is discovered.
+   * @param event The event given to validate
+   */
+  validate: (event) => {
+    // Splitting Settings [Optional]
+    if ('_split' in event && typeof event._split !== 'object') { // eslint-disable-line no-underscore-dangle
+      throw new def.FunctionError('If specified, the "_split" attribute must contain an object')
+    } else {
+      const settings = def.getSettings(event)
+      if (
+        'maxChunkDurationInSeconds' in settings &&
+        (
+          !Number.isInteger(settings.maxChunkDurationInSeconds) ||
+          settings.maxChunkDurationInSeconds < def.MIN_CHUNK_DURATION_IN_SECONDS ||
+          settings.maxChunkDurationInSeconds > def.MAX_CHUNK_DURATION_IN_SECONDS
+        )
+      ) {
+        throw new def.FunctionError('If specified the "_split.maxChunkDurationInSeconds" attribute must be an integer ' +
+          `inclusively between ${def.MIN_CHUNK_DURATION_IN_SECONDS} and ${def.MAX_CHUNK_DURATION_IN_SECONDS}.`)
+      } else if (
+        'maxScriptDurationInSeconds' in settings &&
+        (
+          !Number.isInteger(settings.maxScriptDurationInSeconds) ||
+          settings.maxScriptDurationInSeconds <= 0 ||
+          settings.maxScriptDurationInSeconds > def.MAX_SCRIPT_DURATION_IN_SECONDS
+        )
+      ) {
+        throw new def.FunctionError('If specified the "_split.maxScriptDurationInSeconds" attribute must be an integer ' +
+          `inclusively between 1 and ${def.MAX_SCRIPT_DURATION_IN_SECONDS}.`)
+      } else if (
+        'maxChunkRequestsPerSecond' in settings &&
+        (
+          !Number.isInteger(settings.maxChunkRequestsPerSecond) ||
+          settings.maxChunkRequestsPerSecond <= 0 ||
+          settings.maxChunkRequestsPerSecond > def.MAX_CHUNK_REQUESTS_PER_SECOND
+        )
+      ) {
+        throw new def.FunctionError('If specified the "_split.maxChunkRequestsPerSecond" attribute must be an integer ' +
+          `inclusively between 1 and ${def.MAX_CHUNK_REQUESTS_PER_SECOND}.`)
+      } else if (
+        'maxScriptRequestsPerSecond' in settings &&
+        (
+          !Number.isInteger(settings.maxScriptRequestsPerSecond) ||
+          settings.maxScriptRequestsPerSecond <= 0 ||
+          settings.maxScriptRequestsPerSecond > def.MAX_SCRIPT_REQUESTS_PER_SECOND
+        )
+      ) {
+        throw new def.FunctionError('If specified the "_split.maxScriptRequestsPerSecond" attribute must be an integer ' +
+          `inclusively between 1 and ${def.MAX_SCRIPT_REQUESTS_PER_SECOND}.`)
+      } else if (
+        'timeBufferInMilliseconds' in settings &&
+        (
+          !Number.isInteger(settings.timeBufferInMilliseconds) ||
+          settings.timeBufferInMilliseconds <= 0 ||
+          settings.timeBufferInMilliseconds > def.MAX_TIME_BUFFER_IN_MILLISECONDS
+        )
+      ) {
+        throw new def.FunctionError('If specified the "_split.timeBufferInMilliseconds" attribute must be an integer ' +
+          `inclusively between 1 and ${def.MAX_TIME_BUFFER_IN_MILLISECONDS}.`)
+      }
+    }
+  },
+}
+
+module.exports = impl.validate
---
+++ tests/integration/versioning/0.0.0-to-0.0.1/package-dependency/upgraded/func.js
@@ -0,0 +1,11 @@
+const def = require('./funcDef')
+const exec = require('./funcExec')
+const handle = require('./funcHandle')
+const valid = require('./funcValid')
+
+module.exports = {
+  def,
+  exec,
+  handle,
+  valid,
+}
