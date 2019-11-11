68365e6f2f3cd9bae8d2bf96362fd9dc7a98e2ad
Smith, Greg W <Greg.W.Smith@nordstrom.com>
2019-11-11 11:02:35 -0800

    Remove npm postinstll step.

--- postinstall.js
+++
@@ -1,7 +0,0 @@
-const path = require('path')
-
-const npm = require('./lib/npm')
-
-const installPath = path.join(__dirname, 'lib', 'lambda')
-
-npm.install(installPath) // will throw out and fail install if this times out or returns a non-zero result
